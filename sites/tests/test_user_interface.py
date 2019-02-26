d12081b1ca14a9b3e5b8ecd747b81666da8aeab9
kir <akforjob@yandex.ru>
2019-02-26 22:56:17 +0300

    минимальные базовые тесты

--- sites/tests/test_user_interface.py
+++ sites/tests/test_user_interface.py
@@ -29,3 +29,43 @@ class ArticlesTests(BaseSetupCase):
             'key_words': ['ввоз', 'запрет', 'фтс'],
             'exclude_words': ['проект']
         })
+
+    def test_user_estimation(self):
+        """Тестирование пользовательских оценок статей"""
+        estimation_list = reverse('est-list')
+        response = self.dummy_client.get(estimation_list)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[1].get('user'), self.dummy_user.pk)
+        self.assertEqual(response.data[1].get('article'), self.second_article.pk)
+        self.assertEqual(response.data[1].get('user_estimation'), 'vb')
+        estimation_detail = reverse('est-detail', kwargs={'pk': self.second_estimate.pk})
+        response = self.dummy_client.get(estimation_detail)
+        self.assertEqual(response.status_code, 200)
+        response_data = response.data
+        response_data.pop('id')
+        self.assertEqual(response_data, {
+            'user': self.dummy_user.pk,
+            'article': self.second_article.pk,
+            'user_estimation': 'vb'}
+        )
+        estimation_var = {'user_estimation': 'vg'}
+        response = self.dummy_client.patch(estimation_detail, estimation_var)
+        response_data = response.data
+        response_data.pop('id')
+        self.assertEqual(response.data, {
+            'user': self.dummy_user.pk,
+            'article': self.second_article.pk,
+            'user_estimation': 'vg'}
+        )
+        response = self.dummy_client.delete(estimation_detail)
+        self.assertEqual(response.status_code, 204)
+        new_estimation = {'user': self.dummy_user.pk, 'article': self.second_article.pk, 'user_estimation': 'bd'}
+        response = self.dummy_client.post(estimation_list, new_estimation)
+        self.assertEqual(response.status_code, 201)
+        response_data = response.data
+        response_data.pop('id')
+        self.assertEqual(response_data, {
+            'user': self.dummy_user.pk,
+            'article': self.second_article.pk,
+            'user_estimation': 'bd'}
+        )
